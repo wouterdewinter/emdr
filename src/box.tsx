f0a0f1a8dca7747031774384656944de48c31ff6
Wouter de Winter <wouter@elanza.nl>
2023-09-09 13:00:54 +0200

    cleanup / sound

--- src/box.tsx
+++ src/box.tsx
@@ -1,28 +1,74 @@
+import kick from "./assets/kick.wav";
+import { useState } from "react";
+
 export function Box() {
+  const [active, setActive] = useState(false);
+
+  const speed = 1.4;
+  const audio = new Audio(kick);
+
+  const onAnimationEvent = () => {
+    if (active) {
+      audio.play();
+    }
+    setTimeout(
+      () => {
+        if (active) {
+          audio.play();
+        }
+      },
+      (speed * 1000) / 2,
+    );
+  };
+
   return (
-    <div style={{ width: "1400px", height: "600px", backgroundColor: "#369" }}>
+    <div
+      style={{
+        width: "1400px",
+        height: "600px",
+        backgroundColor: "#369",
+        position: "relative",
+      }}
+    >
       <div
         style={{
-          animation: "bobble infinite 1.5s cubic-bezier(.42,0,.58,1)",
+          animation: active
+            ? `bobble infinite ${speed}s cubic-bezier(.42,0,.58,1)`
+            : undefined,
           willChange: "transform",
         }}
+        onAnimationIteration={onAnimationEvent}
+        onAnimationStart={onAnimationEvent}
       >
         <Dot />
+      </div>
+      <div
+        style={{
+          textAlign: "center",
+          position: "absolute",
+          bottom: "10%",
+        }}
+      >
+        <button onClick={() => setActive(!active)}>
+          {active ? "Stop" : "Start"}
+        </button>
       </div>
     </div>
   );
 }
 
 function Dot() {
+  console.log("render");
   return (
     <div
       style={{
         backgroundColor: "#f33",
+        // filter: "blur(2px)",
         width: "80px",
         height: "80px",
         position: "absolute",
         borderRadius: "40px",
-        border: "solid 2px #fff",
+        // border: "solid 2px #fff",
       }}
     />
   );
